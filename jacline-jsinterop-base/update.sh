4942c4c156f192eb651c6aabf939727f08435c65
Christian Kohlschütter <christian@kohlschutter.com>
2023-11-29 12:49:31 +0100

    Add jacline-jsinterop-base

---
+++ jacline-jsinterop-base/update.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Obtains the jsinterop-base files that are necessary for j2cl/jacline
+#
+# Copyright 2023 by Christian Kohlschütter
+# SPDX-License-Identifier: Apache-2.0
+#
+
+if [[ ! -d jsinterop-base ]]; then
+  git clone https://github.com/google/jsinterop-base.git
+fi
+alias bazelisk=bazel
+cd jsinterop-base
+bazel build java/...
+cd ..
+
+javaOut=src/main/java
+restOut=src/main/resources/META-INF/jacline/generated/
+rm -rf src/main/java
+rm -rf src/main/resources
+mkdir -p "$javaOut" "$restOut"
+
+cp -r jsinterop-base/bazel-bin/java/jsinterop/base/base-j2cl.js/* "$javaOut"/
+cp -r jsinterop-base/bazel-bin/java/jsinterop/base/base-j2cl.js/* "$restOut"/
+
+find src -type d -exec chmod 755 "{}" \;
+find src -type f -exec chmod 644 "{}" \;
+
+find "$javaOut" -type f -not -name "*.java" -exec rm "{}" \;
