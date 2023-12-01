b878508571792da3e4987e6163b2f7cc4765b930
Christian Kohlschütter <christian@kohlschutter.com>
2023-12-01 20:19:02 +0100

    jrejs: Improve ServiceLoader

--- jacline-jrejs/src/fixes/resources/META-INF/jacline/generated/java/util/ServiceLoader.impl.java.js
+++ jacline-jrejs/src/fixes/resources/META-INF/jacline/generated/java/util/ServiceLoader.impl.java.js
@@ -6,13 +6,13 @@ const $Util = goog.require('nativebootstrap.Util$impl');
 
 let Class = goog.forwardDeclare('java.lang.Class$impl');
 let j_l_String = goog.forwardDeclare('java.lang.String$impl');
-let Collection = goog.forwardDeclare('java.util.Collection$impl');
 let Collections = goog.forwardDeclare('java.util.Collections$impl');
 let HashMap = goog.forwardDeclare('java.util.HashMap$impl');
 let Iterator = goog.forwardDeclare('java.util.Iterator$impl');
-let LinkedHashSet = goog.forwardDeclare('java.util.LinkedHashSet$impl');
+let LinkedHashMap = goog.forwardDeclare('java.util.LinkedHashMap$impl');
 let Map = goog.forwardDeclare('java.util.Map$impl');
 let Optional = goog.forwardDeclare('java.util.Optional$impl');
+let $1 = goog.forwardDeclare('java.util.ServiceLoader.$1$impl');
 let Spliterator = goog.forwardDeclare('java.util.Spliterator$impl');
 let Consumer = goog.forwardDeclare('java.util.function.Consumer$impl');
 let j_u_function_Function = goog.forwardDeclare('java.util.function.Function$impl');
@@ -30,17 +30,17 @@ class ServiceLoader extends j_l_Object {
   super();
   /**@type {Class<S>} @nodts*/
   this.f_service__java_util_ServiceLoader_;
-  /**@type {Collection<S>} @nodts*/
+  /**@type {Map<?function():S, S>} @nodts*/
   this.f_implementations__java_util_ServiceLoader_;
  }
  /** @nodts @template S @return {!ServiceLoader<S>} */
- static $create__java_lang_Class__java_util_Collection(/** Class<S> */ service, /** Collection<S> */ implementations) {
+ static $create__java_lang_Class__java_util_Map(/** Class<S> */ service, /** Map<?function():S, S> */ implementations) {
   let $instance = new ServiceLoader();
-  $instance.$ctor__java_util_ServiceLoader__java_lang_Class__java_util_Collection__void(service, implementations);
+  $instance.$ctor__java_util_ServiceLoader__java_lang_Class__java_util_Map__void(service, implementations);
   return $instance;
  }
  /** @nodts */
- $ctor__java_util_ServiceLoader__java_lang_Class__java_util_Collection__void(/** Class<S> */ service, /** Collection<S> */ implementations) {
+ $ctor__java_util_ServiceLoader__java_lang_Class__java_util_Map__void(/** Class<S> */ service, /** Map<?function():S, S> */ implementations) {
   this.$ctor__java_lang_Object__void();
   this.f_service__java_util_ServiceLoader_ = service;
   this.f_implementations__java_util_ServiceLoader_ = implementations;
@@ -50,28 +50,29 @@ class ServiceLoader extends j_l_Object {
   if ($Equality.$same(this.f_implementations__java_util_ServiceLoader_, null)) {
    return /**@type {Iterator<S>}*/ (Collections.m_emptyIterator__java_util_Iterator());
   } else {
-   return this.f_implementations__java_util_ServiceLoader_.m_iterator__java_util_Iterator();
+   let it = this.f_implementations__java_util_ServiceLoader_.m_entrySet__java_util_Set().m_iterator__java_util_Iterator();
+   return /**@type {!$1<S>}*/ ($1.$create__java_util_ServiceLoader__java_util_Iterator(this, it));
   }
  }
  /** @nodts @template S_1 */
- static m_jaclineRegisterService__java_lang_Class__java_lang_Object__void(/** Class<S_1> */ service, /** S_1 */ implementation) {
+ static m_jaclineRegisterService__java_lang_Class__java_util_ServiceLoader_ServiceProvider__void(/** Class<S_1> */ service, /** ?function():S_1 */ implementation) {
   ServiceLoader.$clinit();
-  /**@type {Collection<*>}*/ ($Casts.$to(ServiceLoader.f_JS_SERVICE__java_util_ServiceLoader_.m_computeIfAbsent__java_lang_Object__java_util_function_Function__java_lang_Object(/**@type {Class<*>}*/ (service), j_u_function_Function.$adapt((k) =>{
-   let k_1 = /**@type {Class}*/ ($Casts.$to(k, Class));
-   return /**@type {!LinkedHashSet<*>}*/ (LinkedHashSet.$create__());
-  })), Collection)).add(implementation);
+  /**@type {Map<?function():?, *>}*/ ($Casts.$to(ServiceLoader.f_JS_SERVICE_PROVIDERS__java_util_ServiceLoader_.m_computeIfAbsent__java_lang_Object__java_util_function_Function__java_lang_Object(service.toString(), j_u_function_Function.$adapt((k) =>{
+   let k_1 = /**@type {?string}*/ ($Casts.$to(k, j_l_String));
+   return /**@type {!LinkedHashMap<?function():?, *>}*/ (LinkedHashMap.$create__());
+  })), Map)).put(implementation, null);
  }
  /** @nodts @template S_1 @return {ServiceLoader<S_1>} */
  static m_load__java_lang_Class__java_util_ServiceLoader(/** Class<S_1> */ service) {
   ServiceLoader.$clinit();
-  return /**@type {!ServiceLoader<S_1>}*/ (ServiceLoader.$create__java_lang_Class__java_util_Collection(service, /**@type {Collection<S_1>}*/ ($Casts.$to(ServiceLoader.f_JS_SERVICE__java_util_ServiceLoader_.get(service), Collection))));
+  return /**@type {!ServiceLoader<S_1>}*/ (ServiceLoader.$create__java_lang_Class__java_util_Map(service, /**@type {Map<?function():S_1, S_1>}*/ ($Casts.$to(ServiceLoader.f_JS_SERVICE_PROVIDERS__java_util_ServiceLoader_.get(service.toString()), Map))));
  }
  /** @nodts @return {Optional<S>} */
  m_findFirst__java_util_Optional() {
   if ($Equality.$same(this.f_implementations__java_util_ServiceLoader_, null) || this.f_implementations__java_util_ServiceLoader_.isEmpty()) {
    return /**@type {Optional<S>}*/ (Optional.m_empty__java_util_Optional());
   } else {
-   return /**@type {Optional<S>}*/ (Optional.m_of__java_lang_Object__java_util_Optional(this.f_implementations__java_util_ServiceLoader_.m_iterator__java_util_Iterator().m_next__java_lang_Object()));
+   return /**@type {Optional<S>}*/ (Optional.m_of__java_lang_Object__java_util_Optional(this.m_iterator__java_util_Iterator().m_next__java_lang_Object()));
   }
  }
  /** @override @return {?string} */
@@ -94,7 +95,7 @@ class ServiceLoader extends j_l_Object {
   ServiceLoader.$loadModules();
   j_l_Object.$clinit();
   Iterable.$clinit();
-  ServiceLoader.f_JS_SERVICE__java_util_ServiceLoader_ = /**@type {!HashMap<Class<*>, Collection<*>>}*/ (HashMap.$create__());
+  ServiceLoader.f_JS_SERVICE_PROVIDERS__java_util_ServiceLoader_ = /**@type {!HashMap<?string, Map<?function():?, ?>>}*/ (HashMap.$create__());
  }
  /** @nodts @return {boolean} */
  static $isInstance(/** ? */ instance) {
@@ -103,20 +104,20 @@ class ServiceLoader extends j_l_Object {
  
  /** @nodts */
  static $loadModules() {
-  Class = goog.module.get('java.lang.Class$impl');
   j_l_String = goog.module.get('java.lang.String$impl');
-  Collection = goog.module.get('java.util.Collection$impl');
   Collections = goog.module.get('java.util.Collections$impl');
   HashMap = goog.module.get('java.util.HashMap$impl');
-  LinkedHashSet = goog.module.get('java.util.LinkedHashSet$impl');
+  LinkedHashMap = goog.module.get('java.util.LinkedHashMap$impl');
+  Map = goog.module.get('java.util.Map$impl');
   Optional = goog.module.get('java.util.Optional$impl');
+  $1 = goog.module.get('java.util.ServiceLoader.$1$impl');
   j_u_function_Function = goog.module.get('java.util.function.Function$impl');
   $Equality = goog.module.get('nativebootstrap.Equality$impl');
   $Casts = goog.module.get('vmbootstrap.Casts$impl');
  }
 }
-/**@type {Map<Class<*>, Collection<*>>} @nodts*/
-ServiceLoader.f_JS_SERVICE__java_util_ServiceLoader_;
+/**@type {Map<?string, Map<?function():?, ?>>} @nodts*/
+ServiceLoader.f_JS_SERVICE_PROVIDERS__java_util_ServiceLoader_;
 Iterable.$markImplementor(ServiceLoader);
 $Util.$setClassMetadata(ServiceLoader, 'java.util.ServiceLoader');
 
