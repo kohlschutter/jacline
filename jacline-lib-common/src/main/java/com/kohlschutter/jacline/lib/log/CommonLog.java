b468349760ff448fb3e40972cf7bcbc77d1f35ed
Christian Kohlschütter <christian@kohlschutter.com>
2023-12-04 16:32:57 +0100

    Add missing license header

--- jacline-lib-common/src/main/java/com/kohlschutter/jacline/lib/log/CommonLog.java
+++ jacline-lib-common/src/main/java/com/kohlschutter/jacline/lib/log/CommonLog.java
@@ -1,3 +1,20 @@
+/*
+ * jacline-lib-common
+ *
+ * Copyright 2023 Christian Kohlschütter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.kohlschutter.jacline.lib.log;
 
 import com.kohlschutter.jacline.annotations.JsImplementationProvidedSeparately;
@@ -14,7 +31,7 @@ import jsinterop.annotations.JsType;
  * <p>
  * If there are more paramerters than placeholders, then the remaining ones will be appended to end
  * of the message or provided as additional parameters, depending on the environment. *
- * 
+ *
  * @author Christian Kohlschütter
  */
 @JsType(namespace = "kohlschutter.log", name = "Log")
