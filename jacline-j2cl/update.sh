0c33d0b82b3d7bf26221b4e39452e6c9920658c0
Christian Kohlschütter <christian@kohlschutter.com>
2023-12-05 20:00:01 +0100

    j2cl: Add update.sh script to copy latest dependencies from bazel build

---
+++ jacline-j2cl/update.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -e
+cd "$(dirname $0)"
+cd ../j2cl
+bazelBinDir=$(pwd)/bazel-bin
+alias bazel=bazelisk
+./build_test.sh
+cd - >/dev/null
+srcDir=$(pwd)/src
+
+echo
+
+function syncDirs() {
+  id="$1"
+  src="$2"
+  dst="$srcDir/$id/java"
+  if [[ -z "$1" || -z "$2" ]]; then
+    echo "syncDirs: Invalid arguments" >&2
+    exit 1
+  fi
+  echo "Syncing $1"
+  echo "from $src"
+  echo "to:  $dst"
+
+  if [[ ! -d "$src" ]]; then
+    echo "Missing directory: $src" >&2
+    exit 1
+  fi
+
+  rm -rf "$dst"
+  cp -a "$src" "$dst"
+
+  rm -f $(find "$dst" -name "AutoValue_*.java" -or -name '$AutoValue_*.java')
+}
+
+syncDirs libast \
+	"$bazelBinDir/transpiler/java/com/google/j2cl/transpiler/ast/_javac/ast/libast_sources"
+
+syncDirs liblibrary_info_proto \
+	"$bazelBinDir/transpiler/java/com/google/j2cl/transpiler/backend/libraryinfo/_javac/library_info_proto/liblibrary_info_proto-speed_tmp"
+
+syncDirs libsummary_proto \
+	"$bazelBinDir/transpiler/java/com/google/j2cl/transpiler/backend/wasm/_javac/summary_proto/libsummary_proto-speed_tmp"
+
+syncDirs libworker_protocol_proto \
+	"$bazelBinDir/transpiler/java/com/google/j2cl/common/bazel/_javac/worker_protocol_proto/libworker_protocol_proto-speed_tmp"
+
+echo "Done."
