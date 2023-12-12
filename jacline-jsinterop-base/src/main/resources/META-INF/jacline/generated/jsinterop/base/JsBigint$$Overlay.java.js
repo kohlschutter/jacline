cd3df3105c944a303323c1511935a54c1cc48869
Christian Kohlschütter <christian@kohlschutter.com>
2023-12-13 00:10:16 +0100

    jsinterop-base: Build from source

--- jacline-jsinterop-base/src/main/resources/META-INF/jacline/generated/jsinterop/base/JsBigint$$Overlay.java.js
+++
@@ -1,6 +0,0 @@
-goog.module('jsinterop.base.JsBigint.$Overlay');
-
-
-const $Overlay = goog.require('jsinterop.base.JsBigint.$Overlay$impl');
-/** @nodts */
-exports = $Overlay;
--- jacline-jsinterop-base/src/main/resources/META-INF/jacline/generated/jsinterop/base/JsConstructorFn$$Overlay.java.js
+++
@@ -1,9 +0,0 @@
-goog.module('jsinterop.base.JsConstructorFn.$Overlay');
-
-goog.require('java.lang.Class');
-goog.require('jsinterop.base.InternalPreconditions');
-goog.require('nativebootstrap.Equality');
-
-const $Overlay = goog.require('jsinterop.base.JsConstructorFn.$Overlay$impl');
-/** @nodts */
-exports = $Overlay;
