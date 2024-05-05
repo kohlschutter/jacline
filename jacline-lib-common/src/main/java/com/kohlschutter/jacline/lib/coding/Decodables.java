b8e1353f1b31015800b478eb0f734fdf566673f9
Christian Kohlschütter <christian@kohlschutter.com>
2024-05-05 14:11:30 +0200

    Decodables: Make hasDecoder static

--- jacline-lib-common/src/main/java/com/kohlschutter/jacline/lib/coding/Decodables.java
+++ jacline-lib-common/src/main/java/com/kohlschutter/jacline/lib/coding/Decodables.java
@@ -42,7 +42,7 @@ public final class Decodables {
   }
 
   @JsExport
-  public boolean hasDecoder(String type) {
+  public static boolean hasDecoder(String type) {
     return MAP.containsKey(type);
   }
 
